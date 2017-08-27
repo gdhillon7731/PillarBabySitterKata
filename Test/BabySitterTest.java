b17481132d5f7b8bfd67ecf5087c42ad4d790c5e
gdhillon7731 <dhillon7731@gmail.com>
2017-08-26 22:49:47 -0400

    fixing PayTotals

--- Test/BabySitterTest.java
+++ Test/BabySitterTest.java
@@ -1,29 +1,44 @@
 import com.company.BabySitter;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class BabySitterTest {
 
+    private BabySitter babySitter;
+    int totalPay;
 
+    @Before
+    public void setUp(){
+        babySitter = new BabySitter();
+    }
 
     @Test
-    public void startTime_5pm_EndTime_4pm_BedTime_8pm_PayTotal_132(){
-        BabySitter babySitter = new BabySitter();
-        int totalPay = babySitter.calculatePay(17, 4, 20);
+    public void startTime_5pm_EndTime_4am_BedTime_8pm_PayTotal_132(){
+
+        totalPay = babySitter.calculatePay(17, 4, 20);
         assertEquals(totalPay, 132);
     }
 
     @Test
-    public void startTime_5pm_EndTime_3pm_BedTime_8pm_PayTotal_116(){
-        BabySitter babySitter = new BabySitter();
-        int totalPay = babySitter.calculatePay(17, 3, 20);
+    public void startTime_5pm_EndTime_3am_BedTime_8pm_PayTotal_116(){
+
+        totalPay = babySitter.calculatePay(17, 3, 20);
         assertEquals(totalPay, 116);
     }
     @Test
-    public void startTime_5pm_EndTime_12am_BedTime_9pm_PayTotal_92(){
-        BabySitter babySitter = new BabySitter();
-        int totalPay = babySitter.calculatePay(17, 24, 21);
+    public void startTime_5pm_EndTime_12am_BedTime_9pm_PayTotal_72(){
+
+        totalPay = babySitter.calculatePay(17, 24, 21);
         assertEquals(totalPay, 72);
+    }
+
+    @Test
+    public void startTime_5pm_EndTime_4am_BedTime_12am_148(){
+
+        totalPay = babySitter.calculatePay(17, 4, 24);
+        assertEquals(totalPay, 148);
+
     }
 
 }
